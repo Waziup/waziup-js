bdced7d0b815d1052c13299ce1715339ada71755
Oliver Kipkemei <55626791+OliverMengich@users.noreply.github.com>
2024-01-22 13:42:41 +0300

    build: generated build files

--- dist/src/connect.js
+++ dist/src/connect.js
@@ -2,13 +2,30 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const waziup_1 = require("./waziup");
 async function connect(settings) {
-    if (settings) {
-        var host = settings.host;
+    try {
+        if (settings.host) {
+            var resp = await fetch(settings.host + "/device/id", {
+                headers: {
+                    'Content-Type': 'text/plain'
+                }
+            });
+            if (!resp.ok) {
+                var data = await resp.json();
+                throw `HTTP Error ${resp.status} ${resp.statusText}\n${data}`;
+            }
+            var wazigateID = await resp.text();
+            return {
+                wazigateID,
+                waziup: new waziup_1.Waziup(settings.host),
+            };
+        }
+        else {
+            throw "No host specified";
+        }
     }
-    else {
-        var host = "";
+    catch (error) {
+        throw error;
     }
-    return new waziup_1.Waziup(host, "");
 }
 exports.connect = connect;
 //# sourceMappingURL=connect.js.map
